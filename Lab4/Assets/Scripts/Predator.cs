0236336e212d964e23a9def8d3867062e399d95b
scw000000 <scw000000@gmail.com>
2016-10-09 22:18:30 -0400

    Finished Lab4

---
+++ Lab4/Assets/Scripts/Predator.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Predator : MonoBehaviour {
+   static int m_Fov = 55;
+   private float m_Velocity = 1.0f;
+   private float m_ViewDistance = 20.0f;
+   private GameObject m_CurrentTarget = null;
+   private Vector3 m_RoamingGoal = new Vector3();
+   //private float m_UpdateRoamDir
+	// Use this for initialization
+	void Start ()
+      {
+      }
+	
+	// Update is called once per frame
+	void Update ()
+      {
+      GameObject newBestTarget = null;
+      float besDistance = 0.0f;
+      var bestHuntDir = new Vector3();
+      var predatorDir = gameObject.transform.forward;
+      RaycastHit hitInfo;
+
+      for( int i =  0; i < m_Fov + 1; ++i )
+         {
+         if( Physics.SphereCast( gameObject.transform.position, 0.5f, Quaternion.AngleAxis( i, Vector3.up ) * predatorDir, out hitInfo, m_ViewDistance ) )
+            {
+            if( hitInfo.collider.gameObject.tag.Equals( "Prey" ) &&
+               ( newBestTarget == null || ( hitInfo.distance < besDistance ) ) )
+               {
+               newBestTarget = hitInfo.collider.gameObject;
+               besDistance = hitInfo.distance;
+               bestHuntDir = Quaternion.AngleAxis( i, Vector3.up ) * predatorDir;
+               }
+            }
+         if( Physics.SphereCast( gameObject.transform.position, 0.5f, Quaternion.AngleAxis( -i, Vector3.up ) * predatorDir, out hitInfo, m_ViewDistance ) )
+            {
+            if( hitInfo.collider.gameObject.tag.Equals( "Prey" ) &&
+               ( newBestTarget == null || ( hitInfo.distance < besDistance ) ) )
+               {
+               newBestTarget = hitInfo.collider.gameObject;
+               besDistance = hitInfo.distance;
+               bestHuntDir = Quaternion.AngleAxis( -i, Vector3.up ) * predatorDir;
+               }
+            }
+         }
+      // Override hunting direction for roaming if no prey is found
+      if( newBestTarget != null )
+         {
+         // Target changed
+         if( m_CurrentTarget == null || m_CurrentTarget.GetInstanceID() != newBestTarget.GetInstanceID() )
+            {
+            if( m_CurrentTarget != null )
+               {
+               StopChasing();
+               }
+            StartChasing( newBestTarget );
+            }
+         m_CurrentTarget = newBestTarget;
+         Quaternion newRotation = new Quaternion();
+         bestHuntDir.Normalize();
+         newRotation.SetLookRotation( bestHuntDir, Vector3.up );
+         gameObject.transform.rotation = newRotation;
+         }
+      else
+         {
+         // Tracing failed or has reached roaming posisiton, set a random point and start roaming
+         if( m_CurrentTarget != null || ( Vector3.SqrMagnitude( gameObject.transform.position - m_RoamingGoal ) <= 0.001 ) )
+            {
+            // If was tracing prey, tell prey to stop being traced
+            if( m_CurrentTarget != null )
+               {
+               StopChasing();
+               }
+            SetRoamingGoal();
+            }
+         }
+
+       gameObject.transform.position += m_Velocity * Time.deltaTime * gameObject.transform.forward;
+     // gameObject.transform.Translate( gameObject.transform.forward * m_Velocity * Time.deltaTime );
+
+      Debug.DrawRay( gameObject.transform.position, gameObject.transform.forward, Color.red );
+      Debug.DrawRay( gameObject.transform.position, Quaternion.AngleAxis( m_Fov, Vector3.up ) * predatorDir, Color.red );
+      Debug.DrawRay( gameObject.transform.position, Quaternion.AngleAxis( -m_Fov, Vector3.up ) * predatorDir, Color.red );
+      }
+
+   void SetRoamingGoal()
+      {
+      m_CurrentTarget = null;
+      RaycastHit hitInfo;
+      Vector3 random3DCirleVec;
+      do
+         {
+         var random2DCircleVec = Random.insideUnitCircle.normalized;
+         random3DCirleVec = new Vector3( random2DCircleVec.x, 0, random2DCircleVec.y );
+         }
+      while( Physics.SphereCast( gameObject.transform.position, 0.5f, random3DCirleVec, out hitInfo, 3.0f ) );
+      m_RoamingGoal = transform.position + 3.0f * random3DCirleVec;
+      Quaternion newRotation = new Quaternion();
+      newRotation.SetLookRotation( m_RoamingGoal - gameObject.transform.position, Vector3.up );
+      gameObject.transform.rotation = newRotation;
+      }
+
+   void StartChasing( GameObject prey )
+      {
+      var preyComponent = prey.GetComponent<Prey>();
+      preyComponent.StartChasingSelf( gameObject );
+      }
+
+   void StopChasing()
+      {
+      var preyComponent = m_CurrentTarget.GetComponent<Prey>();
+      preyComponent.StopChasingSelf( gameObject );
+      m_CurrentTarget = null;
+      }
+
+   void StopChasingPrey()
+      {
+      if( m_CurrentTarget != null )
+         {
+         var preyComponent = m_CurrentTarget.GetComponent<Prey>();
+         preyComponent.StopChasingSelf( gameObject );
+         m_CurrentTarget = null;
+         SetRoamingGoal();
+         }
+      }
+
+   void OnTriggerEnter( Collider collider)
+      {
+      Predator predatorComp;
+      if( collider.gameObject.tag.Equals( "Prey" ) )
+         {
+         Debug.Log( "Prey eaten!" );
+         var predators = GameObject.FindGameObjectsWithTag( "Predator" );
+         // tell all predators who are chasing same prey to stop
+         foreach( var predator in predators )
+            {
+            predatorComp = predator.GetComponent<Predator>();
+            if( predatorComp.m_CurrentTarget != null && predatorComp.m_CurrentTarget.GetInstanceID() == collider.gameObject.GetInstanceID() )
+               {
+               predatorComp.StopChasingPrey();
+               }
+            }
+         Destroy( collider.gameObject );
+         }
+
+      }
+}
---
+++ Lab4/Assets/Scripts/Prey.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Prey : MonoBehaviour {
+   private List< GameObject > m_ChasingPredators = new List<GameObject>();
+   private bool m_IsBeingTraced = false;
+   private float m_Velocity = 0.5f;
+   private Vector3 m_RoamingGoal = new Vector3();
+   private float m_EscapeViewLength = 3.0f;
+   // Use this for initialization
+   void Start ()
+      {
+      SetRoamingGoal();
+      }
+	
+	// Update is called once per frame
+	void Update ()
+      {
+      if( m_IsBeingTraced )
+         {
+         Vector3 bestEscapeDir = new Vector3( 0, 0, 0 );
+         Vector3 initialEscapeDir = new Vector3( 0, 0, 0 );
+         foreach( var predator in m_ChasingPredators )
+            {
+            initialEscapeDir += gameObject.transform.position - predator.transform.position;
+            }
+         initialEscapeDir.Normalize();
+         RaycastHit hitInfo;
+         for( int i = 0; i < 180; ++i )
+            {
+            if( !Physics.SphereCast( gameObject.transform.position, 0.5f, Quaternion.AngleAxis( i, Vector3.up ) * initialEscapeDir, out hitInfo, m_EscapeViewLength )
+               || hitInfo.collider.gameObject.tag.Equals( "Prey" ) )
+               {
+               bestEscapeDir = Quaternion.AngleAxis( i, Vector3.up ) * initialEscapeDir;
+               break;
+               }
+            if( !Physics.SphereCast( gameObject.transform.position, 0.5f, Quaternion.AngleAxis( -i, Vector3.up ) * initialEscapeDir, out hitInfo, m_EscapeViewLength )
+               || hitInfo.collider.gameObject.tag.Equals( "Prey" ) )
+               {
+               bestEscapeDir = Quaternion.AngleAxis( -i, Vector3.up ) * initialEscapeDir;
+               break;
+               }
+            }
+
+         Quaternion newRotation = new Quaternion();
+         newRotation.SetLookRotation( bestEscapeDir, Vector3.up );
+         gameObject.transform.rotation = newRotation;
+         }
+      else
+         {
+         // If has reached roaming posisiton, set a random point and start roaming
+         if( Vector3.SqrMagnitude( gameObject.transform.position - m_RoamingGoal ) <= 0.001 )
+            {
+            SetRoamingGoal();
+            }
+         }
+
+      gameObject.transform.position += m_Velocity * Time.deltaTime * gameObject.transform.forward;
+
+      Debug.DrawRay( gameObject.transform.position, gameObject.transform.forward, Color.blue );
+      }
+
+   private void SetRoamingGoal()
+      {
+      Vector3 random3DCirleVec;
+      RaycastHit hitInfo;
+      do
+         {
+         var random2DCircleVec = Random.insideUnitCircle.normalized;
+         random3DCirleVec = new Vector3( random2DCircleVec.x, 0, random2DCircleVec.y );
+         }
+      while( Physics.SphereCast( gameObject.transform.position, 0.5f, random3DCirleVec, out hitInfo, 3.0f ) );
+      m_RoamingGoal = transform.position + 3.0f * random3DCirleVec;
+      Quaternion newRotation = new Quaternion();
+      newRotation.SetLookRotation( m_RoamingGoal - gameObject.transform.position, Vector3.up );
+      gameObject.transform.rotation = newRotation;
+      }
+
+   public void StartChasingSelf( GameObject predator )
+      {
+      m_IsBeingTraced = true;
+      m_ChasingPredators.Add( predator );
+      }
+
+   public void StopChasingSelf( GameObject predator )
+      {
+      m_ChasingPredators.Remove( predator );
+      if( m_ChasingPredators.Count == 0 )
+         {
+         m_IsBeingTraced = false;
+         SetRoamingGoal();
+         }
+      }
+
+
+}
